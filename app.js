bc89d62f0ba89a89a232606e7a50369f4c10dfde
wuzzar321 <sergej.sergejev@gmail.com>
2019-02-12 14:01:31 +0200

    go

--- app.js
+++ app.js
@@ -17,52 +17,74 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-var request = require('request');
 const crypto = require('crypto');
+const https = require('https');
 
 var config = {
     publicKey: "b2f5e41e068fe4c825530ce64952a37542a0be76354d844502fc6f9d52b8f182",
     privateKey: "c56d57db9f5b1e2c10b0a6133758cfa60dc161cee80b9254052e631308fafb84"
 }
 
-function callB1() {
+function signRequest(headers, content) {
+    // Order header items
+    var orderedHeaders = {};
+    Object.keys(headers).sort().forEach(function(key) {
+        orderedHeaders[key] = headers[key];
+    });
+    console.log("Ordered headers:", orderedHeaders);
 
-    var query = {
-        "name": "uabtestena",
-        "countryId": "1"
-    };
+    // Generate data to sign
+    var data = '';
+    Object.keys(orderedHeaders).forEach(function(key) {
+        data += key.toLowerCase() + orderedHeaders[key];
+    });
+    data += content;
+    console.log('Data to sign:', data);
 
-    var hmacDataConstructor = "api-key" + config.publicKey + "content-length" + Buffer.byteLength(JSON.stringify(query)) + "content-typeapplication/json; charset=utf-8" + "platformstandalone" + "time" + Math.floor(new Date() / 1000) + "version2.0.6" + JSON.stringify(query);
-    const hmac = crypto.createHmac('sha512', config.privateKey).update(hmacDataConstructor);
-
-    console.log("hmac length is: " + hmac.digest('hex').length);
-    console.log(hmacDataConstructor);
-
-    var options = {
-        url: "https://www.b1.lt/api/clients/create",
-        qs: query,
-        headers: {
-            "B1-Api-Key": config.publicKey,
-            "B1-Signature": hmac.digest('hex'),
-            "B1-Time": Math.floor(new Date() / 1000),
-            "Content-Type": "application/json; charset=utf-8",
-            "Content-Length": Buffer.byteLength(JSON.stringify(query)),
-            "B1-Version": "2.0.6",
-            "B1-Platform": "standalone"
-        }
-    }
-
-    function callback(error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(body);
-        }
-    }
-
-    request(options, callback);
+    // Generate signature
+    hmac = crypto.createHmac('sha512', config.privateKey).update(data);
+    var signature = hmac.digest('hex');
+    console.log('Signature: ' + signature);
+    return signature;
 }
 
-callB1();
+function buildRequestHeaders(content) {
+    var headers = {
+        "B1-Signature-Version": 2,
+        "B1-Api-Key": config.publicKey,
+        "B1-Time": Math.floor(new Date() / 1000),
+        "Content-Type": "application/json; charset=utf-8",
+        "Content-Length": Buffer.byteLength(content)
+    }
+    headers['B1-Signature'] = signRequest(headers, content);
+    return headers;
+}
+
+function makeRequestToB1() {
+    var dataToSubmit = {
+        name: 'Du gaideliai',
+        locationId: 1
+    };
+    var dataToSubmitAsString = JSON.stringify(dataToSubmit);
+    var headers = buildRequestHeaders(dataToSubmitAsString);
+    var requestOptions = {
+        host: 'www.b1.lt',
+        port: 443,
+        path: '/api/clients/create',
+        method: 'POST',
+        headers: headers
+    };
+    var req = https.request(requestOptions, function(res) {
+        console.log("Response status code:", res.statusCode);
+        console.log("Response headers:", res.headers);
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+           console.log('Response body:', JSON.parse(chunk));
+        });
+    });
+    req.end(dataToSubmitAsString);
+}
+
+makeRequestToB1();
 
 module.exports = app;
